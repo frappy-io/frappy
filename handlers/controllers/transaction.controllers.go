c138d5e0fbb889d90b55f1758021d9c3bac6848b
Siddhant Prateek <siddhantprateek@gmail.com>
2023-02-24 09:35:31 +0530

    transaction controllers need context

---
+++ handlers/controllers/transaction.controllers.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func GetAccount(c *fiber.Ctx) error {
+	return nil
+}
+
+func GetBlock(c *fiber.Ctx) error {
+	return nil
+}
+
+func GetTransaction(c *fiber.Ctx) error {
+	return nil
+}
+
+func GetEvents(c *fiber.Ctx) error {
+	return nil
+}
+
+func BuildTransaction(c *fiber.Ctx) error {
+	return nil
+}
+
+func SignTransaction(c *fiber.Ctx) error {
+	return nil
+}
